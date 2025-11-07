1afbe666ed650207446d230c991e71c621c28263
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2025-11-07 18:52:45 +0100

    Add basic HTTP server to front end

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let server_addr = "localhost:3000";
+    let router = Router::new();
+
+    println!("Launching front end: http://{server_addr:?}");
+    let listener = tokio::net::TcpListener::bind(&server_addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
