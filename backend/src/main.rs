088ea049dd26c547cb34417e2e98869b95955a74
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2025-11-11 09:32:55 +0100

    Add simple HTTP server to back end

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let server_addr = "localhost:3100";
+    let router = Router::new();
+
+    println!("Launching back end: http://{server_addr}");
+    let listener = tokio::net::TcpListener::bind(&server_addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
